6c52c7bc3dbcfa4b0f3078e83ede391f8ad29c60
Sergey Mykhailov <moderya7@gmail.com>
2018-08-15 23:32:08 +0300

    Generate sattelites from code

---
+++ Assets/Scripts/SatelliteGenerationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SatelliteGenerationScript : MonoBehaviour
+{
+    public GameObject Satellite;
+
+    void Start()
+    {
+        Spawn(0, 7);
+        Spawn(3.14f, 5);
+    }
+
+    public void Spawn(float phaseChange, float orbit)
+    {
+        var _satellite = Instantiate(Satellite);
+        var sateliteMoveComponent = _satellite.GetComponent<SateliteMoveScript>();
+        sateliteMoveComponent.orbit = orbit;
+        sateliteMoveComponent.phaseChange = phaseChange;
+    }
+}
