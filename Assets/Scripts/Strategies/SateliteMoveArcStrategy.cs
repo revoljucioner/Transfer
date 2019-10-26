a919661e2025c716e7d425d5a0235b3e2e71f329
revoljucioner <moderya7@gmail.com>
2019-10-27 01:02:50 +0300

    fix

--- Assets/Scripts/Strategies/SateliteMoveArcStrategy.cs
+++ Assets/Scripts/Strategies/SateliteMoveArcStrategy.cs
@@ -28,7 +28,7 @@ namespace Assets.Scripts.RefactoringClasses
 
         public Vector2 CalculatePositionAsFigure()
         {
-            var x = (float)(maxWeight * Sin(Speed * Variables.Time + PhaseChange));
+            var x = (float)(maxWeight * Sin(Speed * Variables.Time + Helper.DegreesToRad(PhaseChange)));
             var y = (float)Sqrt(Pow(Orbit, 2) - Pow(x, 2));
             //сейчас орбита идет по кругу в центре которого находится земная станция. закоменчено движение по орбите в центре которой центр Земли
             //var y = (float)Sqrt(Pow(orbit - EarthCenterY, 2) - Pow(x, 2)) + EarthCenterY;
